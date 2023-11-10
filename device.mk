dd3afbdd6c71d4a4d0d54ca6d138683145408ba6
KiwiBlock27 <143954570+KiwiBlock27@users.noreply.github.com>
2023-11-11 05:26:54 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -443,9 +443,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
-# V4a
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
